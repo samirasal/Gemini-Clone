a978211e9288697d464127e9e0f6f5de8bddc9c8
samirasal <111319083+samirasal@users.noreply.github.com>
2024-06-19 09:56:31 -0400

    saved the prompt in the side bar and added the logic of creating a new chat.

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -2,9 +2,17 @@ import React from "react";
 import "./Sidebar.css";
 import { assets } from "../../assets/assets";
 import { useState } from "react";
+import { useContext } from "react";
+import { Context } from "../../context/Context";
 
 const Sidebar = () => {
   const [extended, setExtended] = useState(false);
+  const { onSent, prevPrompts, setRecentPrompt, newChat } = useContext(Context);
+
+  const loadPrompt = async (prompt) => {
+    setRecentPrompt(prompt);
+    await onSent(prompt);
+  };
   return (
     <div className="sidebar">
       <div className="top">
@@ -14,17 +22,21 @@ const Sidebar = () => {
           src={assets.menu_icon}
           alt=""
         />
-        <div className="new-chat">
+        <div onClick={() => newChat()} className="new-chat">
           <img src={assets.plus_icon} alt="" />
           {extended ? <p>New Chat</p> : null}
         </div>
         {extended ? (
           <div className="recent">
             <p className="recent-title">Recent</p>
-            <div className="recent-entry">
-              <img src={assets.message_icon} alt="" />
-              <p>What is react...</p>
-            </div>
+            {prevPrompts.map((item, index) => {
+              return (
+                <div onClick={() => loadPrompt(item)} className="recent-entry">
+                  <img src={assets.message_icon} alt="" />
+                  <p>{item.slice(0, 18)}...</p>
+                </div>
+              );
+            })}
           </div>
         ) : null}
       </div>
--- src/context/Context.jsx
+++ src/context/Context.jsx
@@ -17,16 +17,30 @@ const ContextProvider = (props) => {
       setResultData((prev) => prev + nextWord);
     }, 75 * index);
   };
+    
+    const newChat = () => {
+        setLoading(false)
+        setShowResult(false)
+    }
 
   const onSent = async (prompt) => {
     setResultData("");
     setLoading(true);
-    setShowResult(true);
-    setRecentPrompt(input);
-
-    const response = await run(input);
+      setShowResult(true);
+      let response;
+      if (prompt !== undefined) {
+          response = await run(prompt);
+          setRecentPrompt(prompt)
+          
+      }
+      else {
+          setPrevPrompts(prev => [...prev, input])
+          setRecentPrompt(input)
+          response = await run(input)
+      }
+    
     let responseArray = response.split("**");
-    let newResponse;
+    let newResponse= " ";
     for (let i = 0; i < responseArray.length; i++) {
       if (i == 0 || i % 2 !== 1) {
         newResponse += responseArray[i];
@@ -54,7 +68,8 @@ const ContextProvider = (props) => {
     loading,
     resultData,
     input,
-    setInput,
+      setInput,
+    newChat
   };
   return (
     <Context.Provider value={contextValue}>{props.children}</Context.Provider>
