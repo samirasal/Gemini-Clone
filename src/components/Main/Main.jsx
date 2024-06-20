11bd6f8b72d5ee8c00326d3e390d63e10e1a69ba
samirasal <111319083+samirasal@users.noreply.github.com>
2024-06-20 08:56:41 -0400

    send icon becomes visible only when there is an input

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -21,7 +21,7 @@ const Main = () => {
         <img src={assets.user_icon2} alt="" />
       </div>
       <div className="main-container">
-        {!showResult ? 
+        {!showResult ? (
           <>
             <div className="greet">
               <p>
@@ -48,23 +48,26 @@ const Main = () => {
               </div>
             </div>
           </>
-        : 
+        ) : (
           <div className="result">
             <div className="result-title">
               <img src={assets.user_icon2} alt="" />
               <p>{recentPrompt}</p>
             </div>
             <div className="result-data">
-                          <img src={assets.gemini_icon} alt="" />
-                          {loading ? <div className="loader">
-                              <hr />
-                              <hr />
-                              <hr />
-                          </div> :
-                              <p dangerouslySetInnerHTML={{ __html: resultData }}></p>}
+              <img src={assets.gemini_icon} alt="" />
+              {loading ? (
+                <div className="loader">
+                  <hr />
+                  <hr />
+                  <hr />
+                </div>
+              ) : (
+                <p dangerouslySetInnerHTML={{ __html: resultData }}></p>
+              )}
             </div>
           </div>
-        }
+        )}
 
         <div className="main-bottom">
           <div className="search-box">
@@ -77,7 +80,9 @@ const Main = () => {
             <div>
               <img src={assets.gallery_icon} alt="" />
               <img src={assets.mic_icon} alt="" />
-              <img onClick={() => onSent()} src={assets.send_icon} alt="" />
+              {input ? (
+                <img onClick={() => onSent()} src={assets.send_icon} alt="" />
+              ) : null}
             </div>
           </div>
           <p className="bottom-info">
